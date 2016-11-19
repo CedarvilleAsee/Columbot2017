d2f264a3e9941cbf13e3bb4a4abb1be4338fffe6
Donald Richard Shade Jr <Donald Richard Shade Jr>
2016-11-19 12:08:33 -0500

    Added an API to control the arms servos
    API includes:
    initialize
    setArmState
    
    the namespace holds two structs holding the servos of the left and the
    right arms. You specify which arm to control by passing it into
    setArmState.

---
+++ OneRobot/OneRobot/arms.cpp
@@ -0,0 +1,21 @@
+#include "constants.h"
+#include "arms.h"
+#include <Arduino.h>
+#include <Servo.h>
+
+void arms::initialize() {
+  //sets up all arm servors
+  leftArm.inner.attach(LEFT_INNER_ARM);
+  leftArm.outer.attach(LEFT_OUTER_ARM);
+  leftArm.claw.attach(LEFT_CLAW);
+  rightArm.inner.attach(RIGHT_INNER_ARM);
+  rightArm.outer.attach(RIGHT_OUTER_ARM);
+  rightArm.claw.attach(RIGHT_CLAW);
+  
+}
+void arms::setArmState(Arm arm, ArmState armState){
+  //sets servos pos
+  arm.inner.write(armState.inner);
+  arm.outer.write(armState.outer);
+  arm.claw.write(armState.claw);        
+}
