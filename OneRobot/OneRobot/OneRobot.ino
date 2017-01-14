6bb23d35e88e4124d1ec10a028a1fe0564c41874
Donald Richard Shade Jr <Donald Richard Shade Jr>
2017-01-14 11:51:18 -0500

    Minor Changes Made
    Arm states were labeled for open/close and claw open/close. Basic
    skeleton was made for the state machine.

--- OneRobot/OneRobot/OneRobot.ino
+++ OneRobot/OneRobot/OneRobot.ino
@@ -35,20 +35,25 @@ void setup(){
   digitalWrite(LEDY, HIGH);
 
   wheels::initialize();
+  arms::initialize(leftArm, rightArm);
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT);
   
   //start serial communication
   //prt.begin(115200);
 
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT);
+
   //serve.attach(RIGHT_CLAW);  
-  arms::initialize(leftArm, rightArm);
+
 //  leftArm.inner.attach(LEFT_INNER_ARM);
 //  leftArm.outer.attach(LEFT_OUTER_ARM);
 //  leftArm.claw.attach(LEFT_CLAW);
 //  rightArm.inner.attach(RIGHT_INNER_ARM);
 //  rightArm.outer.attach(RIGHT_OUTER_ARM);
 //  rightArm.claw.attach(RIGHT_CLAW);
+
+
+
 }
 
 int lastButton1 = 0;
@@ -58,54 +63,64 @@ int lastButton2 = 0;
 bool isClawOpen = true;
 
 void loop() {
-
-  // A bunch of armstates
-  arms::ArmState armOpen;
+  
+  // declaring armstates for the main to use
+  arms::ArmState armOpen;//arm is open and claw is open
   armOpen.claw = 95;
   armOpen.inner = 50;
   armOpen.outer = 50;
-  arms::ArmState clawClose;
+  arms::ArmState clawClose;//arm is open and claw is closed
   clawClose.claw = 73;
   clawClose.inner = 50;
   clawClose.outer = 50;
-  arms::ArmState clawOpen;
-  clawOpen.claw = 95;
-  clawOpen.inner = 50;
-  clawOpen.outer = 50;  
-  arms::ArmState armClosed;
+  arms::ArmState armClosed;//arm is closed and claw is closed
   armClosed.claw = 73;
   armClosed.inner = 150;
   armClosed.outer = 150;
+  arms::ArmState clawOpen;//arm is closed and claw is open
+  clawOpen.claw = 95;
+  clawOpen.inner = 150;
+  clawOpen.outer = 150;  
 
-  // Toggle between all the armstates
-  
-  int button1 = digitalRead(BUTTON1);
-  int button2 = digitalRead(BUTTON2);
 
-  if (button1 != lastButton1) {
-    lastButton1 = button1;
-    if (button1 == LOW) {
-      if (isArmOpen) {
-        isArmOpen = false;
-        setArmState(rightArm, armOpen);
-      }
-      else {
-        isArmOpen = true;
-        setArmState(rightArm, armClosed);
-      }
-    }
-  }
-  if (button2 != lastButton2) {
-    lastButton2 = button2;
-    if (button2 == LOW) {
-      if (isClawOpen) {
-        isClawOpen = false;
-        setArmState(rightArm, clawClose);
-      }
-      else {
-        isClawOpen = true;
-        setArmState(rightArm, clawOpen);        
-      }
-    }
-  }
+
+//State Machine Begginings
+  //update Sensors
+  //update Arms
+  //update wheels
+
+
+
+
+//  // Toggle between all the armstates
+//  
+//  int button1 = digitalRead(BUTTON1);
+//  int button2 = digitalRead(BUTTON2);
+//
+//  if (button1 != lastButton1) {
+//    lastButton1 = button1;
+//    if (button1 == LOW) {
+//      if (isArmOpen) {
+//        isArmOpen = false;
+//        setArmState(rightArm, armOpen);
+//      }
+//      else {
+//        isArmOpen = true;
+//        setArmState(rightArm, armClosed);
+//      }
+//    }
+//  }
+//  if (button2 != lastButton2) {
+//    lastButton2 = button2;
+//    if (button2 == LOW) {
+//      if (isClawOpen) {
+//        isClawOpen = false;
+//        setArmState(rightArm, clawClose);
+//      }
+//      else {
+//        isClawOpen = true;
+//        setArmState(rightArm, clawOpen);        
+//      }
+//    }
+//  }
 }
